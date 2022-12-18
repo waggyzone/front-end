7ddb92a8cc60b7397a4f6e315956509a786bfa57
theertha <theerthabindu26@gmail.com>
2022-12-18 13:52:43 +0530

    feat: petstore

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/web.jpg";
-import "../styles/Home.css";
-
+import BannerImage from "../../assets/web.jpg";
+// import '../../styles/Home.css'
+import './index.css'
 function Home() {
+  
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className="headerContainer">
+      <div className="home__headerContainer">
         <h1> waggy </h1>
         <p> Where being the pet is just Best!</p>
-        <Link to="/menu">
-          <button> PET STORE</button>
+        <Link to="/store">
+          <button className="home__headerContainer__button"> PET STORE</button>
         </Link>
       </div>
     </div>
   );
 }
 
-export default Home;
+export {Home} ;
---
+++ src/pages/Store/Store.js
@@ -0,0 +1,17 @@
+
+import { Card ,CardContent } from '@material-ui/core'
+import React from 'react'
+import './index.css'
+
+const Store = () => {
+  return (
+    <div className="store">
+    <Card>
+  <CardContent>
+  </CardContent>
+  </Card>
+    </div>
+  )
+}
+
+export {Store} 
